5fb1e0cc599f50110fb47bcb39478d6f3ce4c41a
hafiz-awj <hafizaliawj@gmail.com>
2023-08-28 16:24:29 +0500

    firebase connected to app

--- learning-react/src/App.js
+++ learning-react/src/App.js
@@ -1,9 +1,23 @@
+import { getDatabase , ref , set } from "firebase/database";
+import { app } from "./firebase"
 import './App.css';
 
+const db = getDatabase(app);
+
 function App() {
+
+const putDara = () => {
+  set(ref(db, "users/app"), {
+    id: 88753,
+    name: "Hafiz Ali Awj",
+    age: 15,
+  })
+};
+
   return (
     <div className="App">
     <h1>This is a firebase project</h1>
+    <button onClick={putDara}>Set Data</button>
     </div>
   );
 }
